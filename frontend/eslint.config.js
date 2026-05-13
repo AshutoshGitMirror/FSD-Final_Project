6b7b80057cacc8b4d31bfbd98f371e897a5ded0e
AshutoshGitMirror <AshutoshGitMirror@users.noreply.github.com>
2026-05-13 08:52:21 +0530

    fix: eslint config for flat config + no-unused-vars to warn

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,13 +3,23 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
+const reactHooksConfig = reactHooks.configs.flat.recommended
+
 export default [
   { ignores: ['dist'] },
   js.configs.recommended,
-  ...reactHooks.configs.flat.recommended,
   {
+    ...reactHooksConfig,
     files: ['**/*.{js,jsx}'],
-    ...reactRefresh.configs.vite,
+    plugins: {
+      ...reactHooksConfig.plugins,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooksConfig.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]', destructuredArrayIgnorePattern: '^_' }],
+    },
     languageOptions: {
       ecmaVersion: 'latest',
       globals: globals.browser,
@@ -17,9 +27,6 @@ export default [
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
 ]
